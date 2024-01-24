9459ea5ebdf6935f789387ed6cf61654dd2bf01d
Gabe Macri <gabemacru@gmail.com>
2024-01-24 14:41:38 -0500

    install

---
+++ .install/install.sh
@@ -0,0 +1,24 @@
+!bin/bash
+
+echo clone the repo 
+git clone https://github.com/G1A1B1E/music
+
+echo cd into the repo 
+cd music 
+
+echo edit the html 
+cat index.html 
+
+echo locate the files
+ls 
+
+echo cd into the root repo 
+cd ~
+
+echo npm 
+npm install
+
+echo npm start 
+npm run
+
+
