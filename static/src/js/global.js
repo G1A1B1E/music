e5ae479af17f93c4184f8aaaf57b2b4784ad2017
Gabe Macri <gabemacru@gmail.com>
2024-01-24 10:55:55 -0500

    add eveything and footer

---
+++ static/src/js/global.js
@@ -0,0 +1,40 @@
+
+// ver and loaded check
+const ver = 0.1
+console.log(ver)
+console.log('Global Loaded...')
+
+$(function(){
+    $(window).scroll(function(){
+      var winTop = $(window).scrollTop();
+      if(winTop >= 30){
+        $("body").addClass("sticky-header");
+      }else{
+        $("body").removeClass("sticky-header");
+      }//if-else
+    });//win func.
+  });//ready func.
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.footer-link');
+    const movementSpeed = 3;
+    
+    const hoverLink = (event) => {
+        let targetNode = event.target;
+
+        const { left, top, width, height } = targetNode.getBoundingClientRect();
+
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+        
+        let x = (centerX - event.pageX) / movementSpeed;
+        let y = (centerY - event.pageY) / movementSpeed;
+    
+        targetNode.style.setProperty('--x', `${x}px`);
+        targetNode.style.setProperty('--y', `${y}px`);
+    }
+
+    links.forEach(link => {
+        link.addEventListener('mousemove', hoverLink);
+    });
+});
---
+++ static/src/js/index.js
@@ -0,0 +1,7 @@
+
+// ver and loaded check
+const ver = 0.1
+console.log(ver)
+console.log("index Loaded...")
+
+
