1336336abb0d92896c2cb250a86e4162b5da39e1
Gabe Macri <gabemacru@gmail.com>
2024-01-29 15:07:30 -0500

    footer

--- static/pages/pages_src/secretsel.js
+++ static/pages/pages_src/secretsel.js
@@ -5,3 +5,38 @@ console.log(ver)
 console.log("Secret sel loaded...")
 
 
+
+$(function(){
+    $(window).scroll(function(){
+      var winTop = $(window).scrollTop();
+      if(winTop >= 30){
+        $("body").addClass("sticky-header");
+      }else{
+        $("body").removeClass("sticky-header");
+      }//if-else
+    });//win func.
+  });//ready func.
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.footer-link');
+    const movementSpeed = 3;
+    
+    const hoverLink = (event) => {
+        let targetNode = event.target;
+
+        const { left, top, width, height } = targetNode.getBoundingClientRect();
+
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+        
+        let x = (centerX - event.pageX) / movementSpeed;
+        let y = (centerY - event.pageY) / movementSpeed;
+    
+        targetNode.style.setProperty('--x', `${x}px`);
+        targetNode.style.setProperty('--y', `${y}px`);
+    }
+
+    links.forEach(link => {
+        link.addEventListener('mousemove', hoverLink);
+    });
+});
